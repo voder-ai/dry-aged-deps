3af9a212456e04271365f2027c3b96c7f54ff309
Tom Howard <tom.howard@greater.com.au>
2025-11-11 21:23:17 +1100

    chore: remove obsolete scripts from scripts/ directory

--- scripts/modify-lint.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import fs from 'fs';
-// Modify package.json lint script
-const pkgPath = 'package.json';
-const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-pkg.scripts.lint = 'eslint src bin test --ext .js,.cjs';
-fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2) + '\n');
-console.log('Updated lint script in package.json');
