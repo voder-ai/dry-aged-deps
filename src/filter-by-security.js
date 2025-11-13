4de8b438107e4c3d6e18e0503f856d62110968fa
Tom Howard <tom.howard@greater.com.au>
2025-11-13 16:19:11 +1100

    chore: add @ts-nocheck to config-loader, build-rows, filter-by-security modules

--- src/filter-by-security.js
+++ src/filter-by-security.js
@@ -1,3 +1,4 @@
+// @ts-nocheck - TODO: Fix type annotations incrementally
 /**
  * Filter rows by security vulnerabilities.
  * @param {Array} rows - Array of [name, current, wanted, latest, age, depType].
